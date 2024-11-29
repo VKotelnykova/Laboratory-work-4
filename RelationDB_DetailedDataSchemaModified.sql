d9c6a2108ef3aa87bdc546b5308a2cf63ea7edd8
VKotelnykova <127886568+VKotelnykova@users.noreply.github.com>
2024-11-29 02:45:27 +0200

    Update RelationDB_DetailedDataSchemaModified.sql

--- RelationDB_DetailedDataSchemaModified.sql
+++ RelationDB_DetailedDataSchemaModified.sql
@@ -1,86 +1,98 @@
 -- Видалення таблиць з каскадним видаленням можливих описів цілісності
-DROP TABLE IF EXISTS User CASCADE;
-DROP TABLE IF EXISTS SleepSchedule CASCADE;
-DROP TABLE IF EXISTS Sleep CASCADE;
-DROP TABLE IF EXISTS MonitoringMode CASCADE;
-DROP TABLE IF EXISTS Report CASCADE;
-DROP TABLE IF EXISTS Recommendation CASCADE;
-DROP TABLE IF EXISTS OptimalTime CASCADE;
-DROP TABLE IF EXISTS SleepCondition CASCADE;
+DROP TABLE IF EXISTS "user" CASCADE;
+DROP TABLE IF EXISTS sleep_schedule CASCADE;
+DROP TABLE IF EXISTS sleep CASCADE;
+DROP TABLE IF EXISTS monitoring_mode CASCADE;
+DROP TABLE IF EXISTS report CASCADE;
+DROP TABLE IF EXISTS recommendation CASCADE;
+DROP TABLE IF EXISTS optimal_time CASCADE;
+DROP TABLE IF EXISTS sleep_condition CASCADE;
 
--- Створення таблиці User
-CREATE TABLE User (
+-- Створення таблиці "user"
+CREATE TABLE "user" (
     user_id SERIAL PRIMARY KEY, -- Унікальний ідентифікатор користувача
-    sleepSchedule INTEGER NOT NULL -- Зовнішній ключ для зв'язку з таблицею SleepSchedule
+    -- Зовнішній ключ для зв'язку з таблицею sleep_schedule
+    sleep_schedule_id INTEGER NOT NULL
 );
 
--- Створення таблиці SleepSchedule
-CREATE TABLE SleepSchedule (
+-- Створення таблиці sleep_schedule
+CREATE TABLE sleep_schedule (
     schedule_id SERIAL PRIMARY KEY, -- Унікальний ідентифікатор графіка сну
-    bedTime VARCHAR(5) NOT NULL, -- Час відходу до сну (формат: HH:MM)
-    wakeUpTime VARCHAR(5) NOT NULL, -- Час прокидання (формат: HH:MM)
-    user_id INTEGER NOT NULL, -- Зовнішній ключ для User
-    CONSTRAINT fk_user_schedule FOREIGN KEY (user_id) REFERENCES User(user_id)
+    bed_time VARCHAR(5) NOT NULL, -- Час відходу до сну (формат: HH:MM)
+    wake_up_time VARCHAR(5) NOT NULL, -- Час прокидання (формат: HH:MM)
+    user_id INTEGER NOT NULL, -- Зовнішній ключ для "user"
+    CONSTRAINT fk_user_schedule FOREIGN KEY (user_id) REFERENCES "user" (
+        user_id
+    )
 );
 
--- Створення таблиці Sleep
-CREATE TABLE Sleep (
+-- Створення таблиці sleep
+CREATE TABLE sleep (
     sleep_id SERIAL PRIMARY KEY, -- Унікальний ідентифікатор сну
     duration INTEGER CHECK (duration > 0) NOT NULL, -- Тривалість сну в хвилинах
-    wakeCount INTEGER CHECK (wakeCount >= 0) NOT NULL, -- Кількість пробуджень
-    quality FLOAT CHECK (quality >= 0 AND quality <= 100) NOT NULL, -- Якість сну
-    user_id INTEGER NOT NULL, -- Зовнішній ключ для User
-    CONSTRAINT fk_user_sleep FOREIGN KEY (user_id) REFERENCES User(user_id)
+    wake_count INTEGER CHECK (wake_count >= 0) NOT NULL, -- Кількість пробуджень
+    -- Якість сну
+    quality FLOAT CHECK (quality >= 0 AND quality <= 100) NOT NULL,
+    user_id INTEGER NOT NULL, -- Зовнішній ключ для "user"
+    CONSTRAINT fk_user_sleep FOREIGN KEY (user_id) REFERENCES "user" (user_id)
 );
 
--- Створення таблиці MonitoringMode
-CREATE TABLE MonitoringMode (
+-- Створення таблиці monitoring_mode
+CREATE TABLE monitoring_mode (
     mode_id SERIAL PRIMARY KEY, -- Унікальний ідентифікатор режиму моніторингу
-    isActive BOOLEAN NOT NULL, -- Чи активований режим моніторингу
-    user_id INTEGER NOT NULL, -- Зовнішній ключ для User
-    CONSTRAINT fk_user_mode FOREIGN KEY (user_id) REFERENCES User(user_id)
+    mode_name VARCHAR(50) NOT NULL, -- Назва режиму моніторингу
+    user_id INTEGER NOT NULL, -- Зовнішній ключ для "user"
+    CONSTRAINT fk_user_mode FOREIGN KEY (user_id) REFERENCES "user" (user_id)
 );
 
--- Створення таблиці Report
-CREATE TABLE Report (
+-- Створення таблиці report
+CREATE TABLE report (
     report_id SERIAL PRIMARY KEY, -- Унікальний ідентифікатор звіту
-    sleepDuration INTEGER CHECK (sleepDuration > 0) NOT NULL, -- Тривалість сну у звіті
-    wakeCount INTEGER CHECK (wakeCount >= 0) NOT NULL, -- Кількість пробуджень у звіті
-    sleepQuality FLOAT CHECK (sleepQuality >= 0 AND sleepQuality <= 100) NOT NULL, -- Якість сну
-    sleep_id INTEGER NOT NULL, -- Зовнішній ключ для таблиці Sleep
-    CONSTRAINT fk_sleep_report FOREIGN KEY (sleep_id) REFERENCES Sleep(sleep_id)
+    report_date DATE NOT NULL, -- Дата звіту
+    user_id INTEGER NOT NULL, -- Зовнішній ключ для "user"
+    CONSTRAINT fk_user_report FOREIGN KEY (user_id) REFERENCES "user" (
+        user_id
+    )
 );
 
--- Створення таблиці Recommendation
-CREATE TABLE Recommendation (
-    recommendation_id SERIAL PRIMARY KEY, -- Унікальний ідентифікатор рекомендації
-    description TEXT NOT NULL, -- Опис рекомендації
-    report_id INTEGER NOT NULL, -- Зовнішній ключ для таблиці Report
-    CONSTRAINT fk_report_recommendation FOREIGN KEY (report_id) REFERENCES Report(report_id)
+-- Створення таблиці recommendation
+CREATE TABLE recommendation (
+    -- Унікальний ідентифікатор рекомендації
+    recommendation_id SERIAL PRIMARY KEY,
+    recommendation_text TEXT NOT NULL, -- Текст рекомендації
+    user_id INTEGER NOT NULL, -- Зовнішній ключ для "user"
+    CONSTRAINT fk_user_recommendation FOREIGN KEY (
+        user_id
+    ) REFERENCES "user" (user_id)
 );
 
--- Створення таблиці OptimalTime
-CREATE TABLE OptimalTime (
-    optimalTime_id SERIAL PRIMARY KEY, -- Унікальний ідентифікатор оптимального часу
-    recommendedTime VARCHAR(5) NOT NULL, -- Рекомендований час для відходу до сну
-    recommendation_id INTEGER NOT NULL, -- Зовнішній ключ для таблиці Recommendation
-    CONSTRAINT fk_recommendation_optimalTime FOREIGN KEY (recommendation_id) REFERENCES Recommendation(recommendation_id)
+-- Створення таблиці optimal_time
+CREATE TABLE optimal_time (
+    -- Унікальний ідентифікатор оптимального часу
+    optimal_time_id SERIAL PRIMARY KEY,
+    start_time VARCHAR(5) NOT NULL, -- Час початку
+    end_time VARCHAR(5) NOT NULL, -- Час кінця
+    user_id INTEGER NOT NULL, -- Зовнішній ключ для "user"
+    CONSTRAINT fk_user_optimal_time FOREIGN KEY (user_id) REFERENCES "user" (
+        user_id
+    )
 );
 
--- Створення таблиці SleepCondition
-CREATE TABLE SleepCondition (
-    condition_id SERIAL PRIMARY KEY, -- Унікальний ідентифікатор умов сну
-    temperature FLOAT CHECK (temperature >= 0 AND temperature <= 40) NOT NULL, -- Температура в кімнаті
-    lighting VARCHAR(255) NOT NULL, -- Умови освітлення в кімнаті
-    recommendation_id INTEGER NOT NULL, -- Зовнішній ключ для таблиці Recommendation
-    CONSTRAINT fk_recommendation_condition FOREIGN KEY (recommendation_id) REFERENCES Recommendation(recommendation_id)
+-- Створення таблиці sleep_condition
+CREATE TABLE sleep_condition (
+    condition_id SERIAL PRIMARY KEY, -- Унікальний ідентифікатор стану сну
+    condition_description TEXT NOT NULL, -- Опис стану сну
+    user_id INTEGER NOT NULL, -- Зовнішній ключ для "user"
+    CONSTRAINT fk_user_sleep_condition FOREIGN KEY (
+        user_id
+    ) REFERENCES "user" (user_id)
 );
 
 -- Обмеження регулярного виразу для полів типу VARCHAR
 -- Перевірка для email
-ALTER TABLE User ADD CONSTRAINT User_email_check 
-    CHECK (user_email ~* '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$');
+ALTER TABLE "user" ADD CONSTRAINT user_email_check
+CHECK (user_email ~* '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$');
 
 -- Перевірка для телефонного номера
-ALTER TABLE User ADD CONSTRAINT User_phone_check 
-    CHECK (user_phone ~* '^\(\d{3}\)\d{3}-\d{4}$');
+ALTER TABLE "user" ADD CONSTRAINT user_phone_check
+CHECK (user_phone ~* '^\(\d{3}\)\d{3}-\d{4}$');
